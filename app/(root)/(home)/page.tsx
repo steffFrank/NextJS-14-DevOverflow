9bfc169ea7ffeefc94ff4a6921aef09a19139b81
steffFrank <steffpouassi@gmail.com>
2024-02-01 02:53:13 +0100

    feat: Create NoResult component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,36 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { HomePageFilters } from "@/constants/filters";
 import HomeFilters from "@/components/shared/home/HomeFilters";
+import NoResult from "@/components/shared/NoResult";
 
+const questions = [
+  // {
+  //   _id: 1,
+  //   title: "question1",
+  //   tags: [
+  //     { _id: 1, name: "tag1" },
+  //     { _id: 2, name: "tag2" },
+  //   ],
+  //   author: "author one",
+  //   upvotes: 12,
+  //   views: 75,
+  //   answers: 3,
+  //   createdAt: "2021-09-01T12:00:00:000Z",
+  // },
+  // {
+  //   _id: 2,
+  //   title: "question2",
+  //   tags: [
+  //     { _id: 1, name: "tag1" },
+  //     { _id: 2, name: "tag2" },
+  //   ],
+  //   author: "author two",
+  //   upvotes: 13,
+  //   views: 60,
+  //   answers: 2,
+  //   createdAt: "2021-10-01T12:00:00:000Z",
+  // },
+];
 export default function Home() {
   return (
     <>
@@ -33,6 +62,22 @@ export default function Home() {
         />
       </div>
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => {
+            return "QuestionCArd";
+          })
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description=" Be the first to break the silence! Ask a Question and kickstart the
+            discussion. your query could be the next big thing others learn from."
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
