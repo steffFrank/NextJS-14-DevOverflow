5bd2f9fe95f30a0c306ea2fa70bc7e57aadf2acd
steffFrank <steffpouassi@gmail.com>
2024-02-11 01:27:44 +0100

    typo: Remove duplicated  importation of revalidatePath

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -7,7 +7,7 @@ import {
   DeleteUserParams,
   UpdateUserParams,
 } from "./shared.types";
-import { revalidatePath, revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
 
 export const getUserById = async (params: any) => {
