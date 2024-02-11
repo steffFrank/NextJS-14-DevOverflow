6b9b733daa9e62543d3994a0c571cc275318c158
steffFrank <steffpouassi@gmail.com>
2024-02-11 01:24:05 +0100

    style: Disable camel case for space grotesk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 
 import "./globals.css";
